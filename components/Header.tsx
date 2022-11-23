ba2e805f8f7cb07de88f70dae1c491ad266276d5
Munkhbayar <monhbayrm14@gmail.com>
2022-11-23 12:56:43 +0800

    ehlel

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+        <div>
+            <img src="" alt="" />
+        </div>
+         <div>header</div>
+    </header>
+   
+  )
+}
+
+export default Header
